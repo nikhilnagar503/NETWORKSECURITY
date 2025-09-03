cc1124752c0228774a86fe3d51355418d8717b9d
Nikhil <nagarn603@example.com>
2025-09-03 21:31:40 +0530

    data validation part is done

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from network_security.logging.logger import logging
 
 ## configuration of the Data Ingestion Config
 
-from network_security.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
+from network_security.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from network_security.entity.artifact_entity import DataIngestionArtifact
 
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 import sys
 
 
@@ -14,10 +16,19 @@ if __name__=="__main__":
         training_pipeline_config=TrainingPipelineConfig()
         data_ingestion_config=DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
-        data_ingestion = data_ingestion.initiate_data_ingestion()
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info(f"Data ingestion artifact: {data_ingestion}")
+        print(dataingestionartifact)
         
-        print(data_ingestion)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_validation_config,dataingestionartifact)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+            
+        
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- network_security/components/data_ingestion.py
+++ network_security/components/data_ingestion.py
@@ -15,13 +15,13 @@ from typing import List
 from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 load_dotenv()
-
-MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+import certifi
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            self.data_ingestion_config=data_ingestion_config
+            self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -33,17 +33,19 @@ class DataIngestion:
             database_name=self.data_ingestion_config.database_name
             collection_name=self.data_ingestion_config.collection_name
             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
-            collection = self.mongo_client[database_name][collection_name]
+            collection=self.mongo_client[database_name][collection_name]
 
             df=pd.DataFrame(list(collection.find()))
-            
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"],axis=1)
-                
+            
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+        
+        
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
@@ -95,5 +97,5 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
 
